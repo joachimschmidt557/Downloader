bded651c13a961d9fdb15662f3d86754a06780fd
joachimschmidt557 <joachim.schmidt557@outlook.com>
2017-06-24 11:26:26 +0800

    First commit

---
+++ Downloader/main.Designer.vb
@@ -0,0 +1,221 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class main
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(main))
+        Me.ProgressBar = New System.Windows.Forms.ProgressBar()
+        Me.DownloadButton = New System.Windows.Forms.Button()
+        Me.TextBox = New System.Windows.Forms.TextBox()
+        Me.ButtonSettings = New System.Windows.Forms.Button()
+        Me.TextBoxPath = New System.Windows.Forms.TextBox()
+        Me.Label = New System.Windows.Forms.Label()
+        Me.ButtonNew = New System.Windows.Forms.Button()
+        Me.GroupBoxDetails = New System.Windows.Forms.GroupBox()
+        Me.BytesLabel = New System.Windows.Forms.Label()
+        Me.ButtonSelect = New System.Windows.Forms.Button()
+        Me.FolderBrowserDialog = New System.Windows.Forms.FolderBrowserDialog()
+        Me.NotifyIcon = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.ContextMenuStrip = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.OpenDownloaderToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.DownloadFromClipboardToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitDownloaderToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Timer = New System.Windows.Forms.Timer(Me.components)
+        Me.GroupBoxDetails.SuspendLayout()
+        Me.ContextMenuStrip.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ProgressBar
+        '
+        Me.ProgressBar.Location = New System.Drawing.Point(141, 38)
+        Me.ProgressBar.MarqueeAnimationSpeed = 10
+        Me.ProgressBar.Name = "ProgressBar"
+        Me.ProgressBar.Size = New System.Drawing.Size(356, 56)
+        Me.ProgressBar.TabIndex = 0
+        '
+        'DownloadButton
+        '
+        Me.DownloadButton.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.DownloadButton.Location = New System.Drawing.Point(12, 38)
+        Me.DownloadButton.Name = "DownloadButton"
+        Me.DownloadButton.Size = New System.Drawing.Size(123, 56)
+        Me.DownloadButton.TabIndex = 1
+        Me.DownloadButton.Text = "Download"
+        Me.DownloadButton.UseVisualStyleBackColor = True
+        '
+        'TextBox
+        '
+        Me.TextBox.Location = New System.Drawing.Point(12, 12)
+        Me.TextBox.Name = "TextBox"
+        Me.TextBox.Size = New System.Drawing.Size(485, 20)
+        Me.TextBox.TabIndex = 2
+        '
+        'ButtonSettings
+        '
+        Me.ButtonSettings.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.ButtonSettings.Location = New System.Drawing.Point(141, 100)
+        Me.ButtonSettings.Name = "ButtonSettings"
+        Me.ButtonSettings.Size = New System.Drawing.Size(356, 23)
+        Me.ButtonSettings.TabIndex = 3
+        Me.ButtonSettings.Text = "Settings"
+        Me.ButtonSettings.UseVisualStyleBackColor = True
+        '
+        'TextBoxPath
+        '
+        Me.TextBoxPath.Location = New System.Drawing.Point(98, 129)
+        Me.TextBoxPath.Name = "TextBoxPath"
+        Me.TextBoxPath.Size = New System.Drawing.Size(305, 20)
+        Me.TextBoxPath.TabIndex = 4
+        '
+        'Label
+        '
+        Me.Label.AutoSize = True
+        Me.Label.Location = New System.Drawing.Point(12, 129)
+        Me.Label.Name = "Label"
+        Me.Label.Size = New System.Drawing.Size(86, 13)
+        Me.Label.TabIndex = 5
+        Me.Label.Text = "Download Path: "
+        '
+        'ButtonNew
+        '
+        Me.ButtonNew.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.ButtonNew.Location = New System.Drawing.Point(12, 100)
+        Me.ButtonNew.Name = "ButtonNew"
+        Me.ButtonNew.Size = New System.Drawing.Size(123, 23)
+        Me.ButtonNew.TabIndex = 6
+        Me.ButtonNew.Text = "New Downloader"
+        Me.ButtonNew.UseVisualStyleBackColor = True
+        '
+        'GroupBoxDetails
+        '
+        Me.GroupBoxDetails.Controls.Add(Me.BytesLabel)
+        Me.GroupBoxDetails.Location = New System.Drawing.Point(12, 156)
+        Me.GroupBoxDetails.Name = "GroupBoxDetails"
+        Me.GroupBoxDetails.Size = New System.Drawing.Size(484, 50)
+        Me.GroupBoxDetails.TabIndex = 7
+        Me.GroupBoxDetails.TabStop = False
+        Me.GroupBoxDetails.Text = "Details"
+        '
+        'BytesLabel
+        '
+        Me.BytesLabel.AutoSize = True
+        Me.BytesLabel.Location = New System.Drawing.Point(10, 21)
+        Me.BytesLabel.Name = "BytesLabel"
+        Me.BytesLabel.Size = New System.Drawing.Size(33, 13)
+        Me.BytesLabel.TabIndex = 0
+        Me.BytesLabel.Text = "Bytes"
+        '
+        'ButtonSelect
+        '
+        Me.ButtonSelect.Location = New System.Drawing.Point(409, 129)
+        Me.ButtonSelect.Name = "ButtonSelect"
+        Me.ButtonSelect.Size = New System.Drawing.Size(87, 23)
+        Me.ButtonSelect.TabIndex = 8
+        Me.ButtonSelect.Text = "Select"
+        Me.ButtonSelect.UseVisualStyleBackColor = True
+        '
+        'FolderBrowserDialog
+        '
+        Me.FolderBrowserDialog.Description = "Select a download location"
+        '
+        'NotifyIcon
+        '
+        Me.NotifyIcon.ContextMenuStrip = Me.ContextMenuStrip
+        Me.NotifyIcon.Icon = CType(resources.GetObject("NotifyIcon.Icon"), System.Drawing.Icon)
+        Me.NotifyIcon.Text = "Downloader"
+        Me.NotifyIcon.Visible = True
+        '
+        'ContextMenuStrip
+        '
+        Me.ContextMenuStrip.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.OpenDownloaderToolStripMenuItem, Me.DownloadFromClipboardToolStripMenuItem, Me.ExitDownloaderToolStripMenuItem})
+        Me.ContextMenuStrip.Name = "ContextMenuStrip"
+        Me.ContextMenuStrip.Size = New System.Drawing.Size(213, 70)
+        '
+        'OpenDownloaderToolStripMenuItem
+        '
+        Me.OpenDownloaderToolStripMenuItem.Name = "OpenDownloaderToolStripMenuItem"
+        Me.OpenDownloaderToolStripMenuItem.Size = New System.Drawing.Size(212, 22)
+        Me.OpenDownloaderToolStripMenuItem.Text = "Open Downloader"
+        '
+        'DownloadFromClipboardToolStripMenuItem
+        '
+        Me.DownloadFromClipboardToolStripMenuItem.Name = "DownloadFromClipboardToolStripMenuItem"
+        Me.DownloadFromClipboardToolStripMenuItem.Size = New System.Drawing.Size(212, 22)
+        Me.DownloadFromClipboardToolStripMenuItem.Text = "Download from Clipboard"
+        '
+        'ExitDownloaderToolStripMenuItem
+        '
+        Me.ExitDownloaderToolStripMenuItem.Name = "ExitDownloaderToolStripMenuItem"
+        Me.ExitDownloaderToolStripMenuItem.Size = New System.Drawing.Size(212, 22)
+        Me.ExitDownloaderToolStripMenuItem.Text = "Exit Downloader"
+        '
+        'Timer
+        '
+        Me.Timer.Enabled = True
+        Me.Timer.Interval = 2000
+        '
+        'main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(509, 218)
+        Me.Controls.Add(Me.ButtonSelect)
+        Me.Controls.Add(Me.GroupBoxDetails)
+        Me.Controls.Add(Me.ButtonNew)
+        Me.Controls.Add(Me.Label)
+        Me.Controls.Add(Me.TextBoxPath)
+        Me.Controls.Add(Me.ButtonSettings)
+        Me.Controls.Add(Me.TextBox)
+        Me.Controls.Add(Me.DownloadButton)
+        Me.Controls.Add(Me.ProgressBar)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "main"
+        Me.Text = "Downloader"
+        Me.GroupBoxDetails.ResumeLayout(False)
+        Me.GroupBoxDetails.PerformLayout()
+        Me.ContextMenuStrip.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ProgressBar As System.Windows.Forms.ProgressBar
+    Friend WithEvents DownloadButton As System.Windows.Forms.Button
+    Friend WithEvents TextBox As System.Windows.Forms.TextBox
+    Friend WithEvents ButtonSettings As System.Windows.Forms.Button
+    Friend WithEvents TextBoxPath As System.Windows.Forms.TextBox
+    Friend WithEvents Label As System.Windows.Forms.Label
+    Friend WithEvents ButtonNew As System.Windows.Forms.Button
+    Friend WithEvents GroupBoxDetails As System.Windows.Forms.GroupBox
+    Friend WithEvents BytesLabel As System.Windows.Forms.Label
+    Friend WithEvents ButtonSelect As System.Windows.Forms.Button
+    Friend WithEvents FolderBrowserDialog As System.Windows.Forms.FolderBrowserDialog
+    Friend WithEvents NotifyIcon As System.Windows.Forms.NotifyIcon
+    Friend WithEvents ContextMenuStrip As System.Windows.Forms.ContextMenuStrip
+    Friend WithEvents OpenDownloaderToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents DownloadFromClipboardToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ExitDownloaderToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents Timer As System.Windows.Forms.Timer
+
+End Class
---
+++ Downloader/main.vb
@@ -0,0 +1,242 @@
+﻿Imports System.Net
+Imports System.ComponentModel
+
+Public Class main
+    Dim DownloadDirectory As System.IO.DirectoryInfo
+    Dim mainwindow As Boolean = False
+    Dim downloadfnotification As New DownloadFinished
+    Dim downloadedfile As String
+    Dim SW As Stopwatch
+    Dim downloadrunning As Boolean = False
+
+    Private Sub main_FormClosing(sender As Object, e As System.Windows.Forms.FormClosingEventArgs) Handles Me.FormClosing
+        If downloadrunning = True Then
+            If MsgBox("Do you really want to close? All running Downloads will be canceled. ", MsgBoxStyle.OkCancel, "Close") = MsgBoxResult.Ok Then
+                e.Cancel = False
+            Else
+                e.Cancel = True
+            End If
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        Me.Height = 158
+        Label.Visible = False
+        TextBoxPath.Visible = False
+        TextBoxPath.Text = "D:\Downloader\"
+        BytesLabel.Text = "No Download running"
+    End Sub
+
+    Private Sub DownloadButton_Click(sender As System.Object, e As System.EventArgs) Handles DownloadButton.Click
+        Try
+            'If DownloadDirectory.Exists = True Then
+            If TextBox.Text <> "" And TextBoxPath.Text <> "" Then
+                'Disable all buttons first
+                DownloadButton.Enabled = False
+                'ButtonSettings.Enabled = False
+                TextBoxPath.Enabled = False
+                TextBox.Enabled = False
+                'Now Declare
+                Dim s As New WebClient
+                AddHandler s.DownloadProgressChanged, AddressOf DownloadProgressChanged
+                AddHandler s.DownloadFileCompleted, AddressOf Finished
+                'Now Evaluate
+                Dim shortfilename As String
+                Dim localfile As String
+                shortfilename = EvaluateInternetSourceFile(TextBox.Text)
+                localfile = EvaluateLocalFile(shortfilename)
+                'Now show the local file in the  settings button
+                'ButtonSettings.Text = localfile
+                downloadedfile = localfile
+                'Now check the internet connection
+                If CheckForInternetConnection() = False Then
+                    MsgBox("There is no internet connection. ", MsgBoxStyle.Exclamation, "Error")
+                    Exit Sub
+                End If
+                'Start the Stopwatch
+                SW = Stopwatch.StartNew
+                'Show balloon
+                NotifyIcon.ShowBalloonTip(2000, "Downloader", "Your download is starting.", ToolTipIcon.Info)
+                'Now Download
+                downloadrunning = True
+                s.DownloadFileAsync(New Uri(TextBox.Text), localfile)
+            Else
+                downloadrunning = False
+                MsgBox("An input is missing!", MsgBoxStyle.Exclamation, "Downloader")
+            End If
+            'Else
+            'DownloadDirectory.Create()
+            'MsgBox("Please Restart the Download. ")
+            'End If
+        Catch ex As Exception
+            MsgBox("An error occured. " + vbCrLf + "Details: " + ex.ToString, MsgBoxStyle.Critical, "Error!")
+        End Try
+    End Sub
+
+    Private Sub ButtonSettings_Click(sender As System.Object, e As System.EventArgs) Handles ButtonSettings.Click
+        If ButtonSettings.Text = "Settings" Then
+            If Label.Visible = True Then
+                Label.Visible = False
+                TextBoxPath.Visible = False
+                Me.Height = 158
+            Else
+                Label.Visible = True
+                TextBoxPath.Visible = True
+                Me.Height = 246
+            End If
+        Else
+            Try
+                System.Diagnostics.Process.Start(ButtonSettings.Text)
+            Catch ex As Exception
+                MsgBox("An error occured. " + vbCrLf + "Details: " + ex.ToString, MsgBoxStyle.Critical, "Error!")
+            End Try
+        End If
+    End Sub
+
+    Public Function EvaluateInternetSourceFile(ByVal longName As String) As String
+        Dim fnPeices() As String = longName.Split("/")
+        Dim filename As String = ""
+        filename = fnPeices(fnPeices.Length - 1)
+        Return filename
+    End Function
+
+    Public Function EvaluateLocalFile(ByVal filename As String) As String
+        Dim localfilea As String = ""
+        localfilea = TextBoxPath.Text + filename
+        Return localfilea
+    End Function
+
+    Public Sub DownloadProgressChanged(sender As System.Object, e As DownloadProgressChangedEventArgs)
+        Dim Speed As Integer
+        'Show progress
+        ProgressBar.Value = e.ProgressPercentage
+        'Show percentage
+        DownloadButton.Text = e.ProgressPercentage.ToString + "%"
+        '... in window title
+            Me.Text = "Downloader - " + e.ProgressPercentage.ToString + "%"
+        'Calculate speed
+        Speed = CInt(e.BytesReceived / SW.ElapsedMilliseconds)
+        'Update NotifyIcon
+        NotifyIcon.Text = "Downloader" + vbCrLf + e.ProgressPercentage.ToString + "%"
+        'Show speed & Bytes transferred & bytes remaining
+        BytesLabel.Text = e.BytesReceived.ToString + " Bytes of " + e.TotalBytesToReceive.ToString + " Bytes @" + Speed.ToString + "KB/sec"
+    End Sub
+
+    Public Sub Finished(sender As System.Object, e As AsyncCompletedEventArgs)
+        'Set downloadrunning to false
+        downloadrunning = False
+        'Reset everything
+        DownloadButton.Enabled = True
+        DownloadButton.Text = "Download"
+        ButtonSettings.Enabled = True
+        TextBox.Enabled = True
+        TextBoxPath.Enabled = True
+        'Show window text
+        Me.Text = "Downloader - Finished"
+        BytesLabel.Text = "No download running"
+        'Update NotifyIcon
+        NotifyIcon.Text = "Downloader" + vbCrLf + "Download finished"
+        'Show download notification
+        If downloadfnotification.ShowDialog() = Windows.Forms.DialogResult.OK Then
+            Try
+                System.Diagnostics.Process.Start(downloadedfile)
+            Catch ex As Exception
+                MsgBox("An error occured. " + vbCrLf + "Details: " + ex.ToString, MsgBoxStyle.Critical, "Error!")
+            End Try
+        End If
+    End Sub
+
+    Private Sub ButtonNew_Click(sender As System.Object, e As System.EventArgs) Handles ButtonNew.Click
+        Try
+            Diagnostics.Process.Start(Application.ExecutablePath)
+        Catch ex As Exception
+            MsgBox("An error occured. " + vbCrLf + ex.ToString)
+        End Try
+    End Sub
+
+    Public Function IsConnectionAvailable() As Boolean
+            Me.Text = "Downloader - Checking internet connection..."
+            If My.Computer.Network.IsAvailable = False Then
+                Return False
+            End If
+            ' Returns True if connection is available 
+            ' Replace www.yoursite.com with a site that
+            ' is guaranteed to be online - perhaps your 
+            ' corporate site, or microsoft.com
+            Dim objUrl As New System.Uri("http://www.google.com/")
+            ' Setup WebRequest
+            Dim objWebReq As System.Net.WebRequest
+            objWebReq = System.Net.WebRequest.Create(objUrl)
+            Dim objResp As System.Net.WebResponse
+            Try
+                ' Attempt to get response and return True
+                objResp = objWebReq.GetResponse
+                objResp.Close()
+                objWebReq = Nothing
+                Return True
+            Catch ex As Exception
+                ' Error, exit and return False
+                objResp.Close()
+                objWebReq = Nothing
+                Return False
+            End Try
+    End Function
+
+    Public Function CheckForInternetConnection() As Boolean
+        If mainwindow = False Then
+            Me.Text = "Downloader - Checking internet connection..."
+        Else
+            Me.Text = "Main Downloader - Checking internet connection..."
+        End If
+        If My.Computer.Network.IsAvailable = False Then
+            Return False
+        End If
+        Try
+            Using client = New WebClient()
+                Using stream = client.OpenRead("http://www.google.com")
+                    Return True
+                End Using
+            End Using
+        Catch
+            Return False
+        End Try
+    End Function
+
+    Private Sub ButtonSelect_Click(sender As System.Object, e As System.EventArgs) Handles ButtonSelect.Click
+        If FolderBrowserDialog.ShowDialog() = Windows.Forms.DialogResult.OK Then
+            TextBoxPath.Text = FolderBrowserDialog.SelectedPath
+        End If
+    End Sub
+
+    Private Sub OpenDownloaderToolStripMenuItem_Click(sender As System.Object, e As System.EventArgs) Handles OpenDownloaderToolStripMenuItem.Click
+        Me.WindowState = FormWindowState.Normal
+    End Sub
+
+    Private Sub DownloadFromClipboardToolStripMenuItem_Click(sender As System.Object, e As System.EventArgs) Handles DownloadFromClipboardToolStripMenuItem.Click
+        If Clipboard.ContainsText() Then
+            TextBox.Text = Clipboard.GetText()
+            DownloadButton_Click(Nothing, Nothing)
+        Else
+            MsgBox("An error occured. " + vbCrLf + "Details: There is no text in the clipboard. ", MsgBoxStyle.Critical, "Error!")
+        End If
+    End Sub
+
+    Private Sub ExitDownloaderToolStripMenuItem_Click(sender As System.Object, e As System.EventArgs) Handles ExitDownloaderToolStripMenuItem.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Timer_Tick(sender As System.Object, e As System.EventArgs) Handles Timer.Tick
+        Try
+            Dim sw As New IO.StreamWriter(TextBoxPath.Text + "\.downloader")
+            If DownloadButton.Enabled = True Then
+                sw.WriteLine("a true")
+                sw.WriteLine(downloadedfile)
+                sw.WriteLine(ProgressBar.Value)
+            Else
+                sw.WriteLine("a false")
+            End If
+            sw.Close()
+        Catch
+        End Try
+    End Sub
+End Class
